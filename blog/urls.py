77cba92a04ea9a1659084b70ac6b0e54b08fbdb9
Behnam <behnamfattahzadeh90@gmail.com>
2020-06-14 09:25:21 +0430

    add dynamic

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.IndexPage.as_view(), name='index')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- blogpy/settings.py
+++ blogpy/settings.py
@@ -126,3 +126,7 @@ STATICFILES_DIRS = (
 )
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 CKEDITOR_UPLOAD_PATH = "uploads/"
+
+# Media Settings
+MEDIA_URL = 'files/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
